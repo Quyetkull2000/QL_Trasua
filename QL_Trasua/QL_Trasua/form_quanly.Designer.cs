e1751d17fa6d5b960223b82756460d139059ee45
Quyetkull <62789782+Quyetkull@users.noreply.github.com>
2021-01-04 00:02:43 +0700

    them project QLTrasua

---
+++ QL_Trasua/QL_Trasua/form_quanly.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace QL_Trasua
+{
+    partial class form_quanly
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // form_quanly
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 477);
+            this.Name = "form_quanly";
+            this.Text = "form_quanly";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
