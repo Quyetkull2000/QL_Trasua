4e1a2749bdcb5eb4e92d6c9e4c5b017123aeddb3
Quyetkull <62789782+Quyetkull@users.noreply.github.com>
2021-01-05 21:05:38 +0700

    toi up project

---
+++ QLQuanTrasua/QL_Trasua.sql
@@ -0,0 +1,89 @@
+﻿CREATE DATABASE QLTraSua
+GO
+
+USE QLTraSua
+GO
+
+-- Food
+-- Table
+-- FoodCategory
+-- Account
+-- Bill
+-- BillInfo
+
+CREATE TABLE TableFood
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Bàn chưa có tên',
+	status NVARCHAR(100) NOT NULL DEFAULT N'Trống'	-- Trống || Có người
+)
+GO
+
+CREATE TABLE Account
+(
+	UserName NVARCHAR(100) PRIMARY KEY,	
+	DisplayName NVARCHAR(100) NOT NULL DEFAULT N'Qkull',
+	PassWord NVARCHAR(1000) NOT NULL DEFAULT 0,
+	Type INT NOT NULL  DEFAULT 0 -- 1: admin && 0: staff
+)
+GO
+
+CREATE TABLE FoodCategory
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Chưa đặt tên'
+)
+GO
+
+CREATE TABLE Food
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Chưa đặt tên',
+	idCategory INT NOT NULL,
+	price FLOAT NOT NULL DEFAULT 0
+	
+	FOREIGN KEY (idCategory) REFERENCES dbo.FoodCategory(id)
+)
+GO
+
+CREATE TABLE Bill
+(
+	id INT IDENTITY PRIMARY KEY,
+	DateCheckIn DATE NOT NULL DEFAULT GETDATE(),
+	DateCheckOut DATE,
+	idTable INT NOT NULL,
+	status INT NOT NULL DEFAULT 0 -- 1: đã thanh toán && 0: chưa thanh toán
+	
+	FOREIGN KEY (idTable) REFERENCES dbo.TableFood(id)
+)
+GO
+
+CREATE TABLE BillInfo
+(
+	id INT IDENTITY PRIMARY KEY,
+	idBill INT NOT NULL,
+	idFood INT NOT NULL,
+	count INT NOT NULL DEFAULT 0
+	
+	FOREIGN KEY (idBill) REFERENCES dbo.Bill(id),
+	FOREIGN KEY (idFood) REFERENCES dbo.Food(id)
+)
+GO
+
+Insert into Account values 
+	('admin','Admin','admin123',1),
+	('Quyetkull',N'Quản lý','Quyetkull2000',0),
+	('Quangbess',N'Nhân viên','Quangbees2000',0)
+	
+GO
+
+--Lấy thông tin người dùng có username là giá trị nhập vào
+CREATE PROC sp_GetAccount @username nvarchar(100)
+AS
+	BEGIN
+		Select * from Account where UserName= @username
+	END
+GO
+
+EXEC sp_GetAccount @username= N'Quyetkull' 
+Select * from Account where UserName= N'Quyetkull' and PassWord= '1'
