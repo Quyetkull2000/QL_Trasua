2bc0a8fe81e3b27de204dc16a3f33f832281479a
quangbees <1851171628@e.tlu.edu.vn>
2021-01-09 20:55:31 +0700

    ala

--- QLTrasua/QL_Trasua.sql
+++ QLTrasua/QL_Trasua.sql
@@ -273,3 +273,59 @@ Begin
 End
 
 Go
+
+
+delete BillInfo
+delete Bill
+
+CREATE TRIGGER UTG_UpdateBillInfo
+On BillInfo for insert, update
+as
+begin
+	Declare @idBill int
+	Select @idBill = idBill from inserted
+	Declare @idTable int
+	Select @idTable = idTable from Bill where id = @idBill and status = 0
+
+	Update TableFood set status = N'Có người' where id = @idTable
+End
+go
+
+Create TRIGGER UTG_UpdateBill
+On Bill for update
+as
+begin
+	declare @idBill int
+	select @idBill = id from inserted
+	Declare @idTable int
+	Select @idTable = idTable from Bill where id = @idBill 
+
+	Declare @count int = 0
+	select @count = COUNT(*) from Bill where idTable = @idTable and status = 0
+	if (@count = 0)
+		update TableFood set status = N'Trống' where id = @idTable
+end 
+go
+
+
+Create trigger UTG_DeleteBillInfo
+on BillInfo for delete
+as
+begin
+	declare @idBillInfo int
+	declare @idBill int
+	select @idBillInfo = id, @idBill = Deleted.idBill from Deleted
+
+	declare @idTable int
+	select @idTable = @idTable from Bill where id = @idBill
+	
+	declare @count int = 0
+
+	select @count = Count(*) from BillInfo as bi, Bill as b where b.id = bi.idBill and b.id = @idBill and b.id = @idBill and b.status = 0 
+
+	if (@count = 0)
+		update TableFood set status = N'Trống' Where id = @idTable
+end
+go
+
+
